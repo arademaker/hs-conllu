f42cbac4561bbbb8e99cc7fce5d0ea46f0b6adef
odanoburu <bcclaro@gmail.com>
2018-01-06 15:15:46 -0200

    - add maybeEmpty combinator - misc fixes - organize code

--- ConlluReader.hs
+++ ConlluReader.hs
@@ -1,3 +1,5 @@
+module ConlluReader where
+
 {--
 
  CoNLL-U parsed file reader.  CoNLL format is defined here:
@@ -14,25 +16,30 @@
 
 --}
 
-module ConlluReader where
+---
+-- imports
 
+-- lib imports
 import Ud
 
---import Control.Applicative
+-- stdlib
+import Control.Monad
 import Data.Char
 import Data.List
---import Data.List.Split
 import Data.Maybe
-import Control.Monad
+import System.Environment
 import System.IO
+
+-- hackage
 import Text.Parsec hiding (token,tokens)
 import Text.Parsec.Combinator
-import Text.ParserCombinators.Parsec.Char
 import Text.Parsec.String
+import Text.ParserCombinators.Parsec.Char
 
-
+---
+-- conllu parsers
 document :: Parser [Sentence]
-document = do ss <- endBy1 sentence blankLine
+document = do ss <- sepBy1 sentence blankLine
               eof
               return ss
 
@@ -42,70 +49,54 @@ blankLine = do litSpaces -- shouldn't exist, but no problem being lax
                newline
                return ()
 
-litSpaces :: Parser ()
--- because spaces consumes \t and \n
-litSpaces = do skipMany $ char ' '
-
 sentence :: Parser Sentence
-sentence = do cs <- many comment
-              ts <- many1 token
-              return Sentence { meta = cs, tokens = ts }
+sentence = do liftM2 Sentence (many comment) (many1 token)
 
 comment :: Parser Comment
 comment = do char '#'
-             stringPair
-
-stringPair :: Parser (String, String)
-stringPair = do
-  parsekeyValue '=' (stringNot "=\n\t ") stringWSpaces
-
-keyValue :: Char -> Parser a -> Parser b -> Parser (a, b)
-keyValue sep p q = do key   <- p
-                      char sep
-                      value <- q
-                      return (p, q)
-
-stringNot :: String -> Parser String
--- [ ] second litSpaces is redundant
-stringNot s = do symbol . many1 $ noneOf s
-
-symbol :: Parser a -> Parser a
-symbol p = do litSpaces
-              x <- p
-              litSpaces
-              return x
+             c <- commentPair
+             newline
+             return c
 
 token :: Parser Token
 -- how to parse tabs elegantly?
 -- new combinator? no.
 -- check liftM5 and above for no variable parsing, see
 -- graham's book
-token = do ix    <- sTokenId
-           ixSep <- optionMaybe $ choice [char '-', char '.']
+token = do ix    <- index
+           ixSep <- optionMaybe indexSep
            ixEnd <- optionMaybe index
            tab
-           fo    <- formP
+           fo    <- maybeEmpty formP
            tab
-           l     <- lemmaP
+           l     <- maybeEmpty lemmaP
            tab
-           up    <- upostagP
+           up    <- maybeEmpty upostagP
            tab
            xp    <- xpostagP
            tab
            fe    <- featsP
            tab
-           h     <- depheadP
+           h     <- maybeEmpty depheadP
            tab
-           dr    <- deprelP
+           dr    <- maybeEmpty deprelP
            tab
            d     <- depsP
            tab
            m     <- miscP
-           return $ mkToken ix ixSep ixEnd fo l up xp f h dr d m
+           newline
+           return $ mkToken ix ixSep ixEnd fo l up xp fe h dr d m
+
+emptyField :: Parser (Maybe a)
+emptyField = do char '_'
+                return Nothing
 
 index :: Parser Index
 index = do ix <- many1 digit
            return (read ix :: Index)
+
+indexSep :: Parser IxSep
+indexSep = do choice [char '-', char '.']
 
 formP :: Parser Form
 formP = do stringWSpaces
@@ -113,141 +104,80 @@ formP = do stringWSpaces
 lemmaP :: Parser Lemma
 lemmaP = do stringWSpaces
 
-stringWSpaces :: Parser String
-stringWSpaces = do stringNot "\t\n"
-
 upostagP :: Parser PosTag
-upostagP = do liftM mkPosTag $ stringWOSpaces
+upostagP = do liftM mkPosTag stringWOSpaces
 
 xpostagP :: Parser Xpostag
-xpostagP = do stringWOSpaces
+xpostagP = do maybeEmpty stringWOSpaces
+
+featsP :: Parser Feats
+featsP = do listP $ listPair '=' (stringNot "=") (stringNot "\t|")
+
+depheadP :: Parser Index
+depheadP = do symbol index
+
+deprelP :: Parser DepRel
+deprelP = do liftM mkDepRel (many1 letter)
+
+depsP :: Parser Deps
+depsP = do listP $ listPair ':' index deprelP
+
+miscP :: Parser Misc
+miscP = do maybeEmpty stringWSpaces
+
+---
+-- utility parsers
+litSpaces :: Parser ()
+-- because spaces consumes \t and \n
+litSpaces = do skipMany $ char ' '
+
+commentPair :: Parser (String, String)
+commentPair = do
+  keyValue '=' (stringNot "=\n \t") stringWSpaces
+
+listPair :: Char -> Parser a -> Parser b -> Parser [(a, b)]
+listPair sep p q = do
+  sepBy1 (keyValue sep p q) (char '|')
+
+stringNot :: String -> Parser String
+-- [ ] second litSpaces in symbol is redundant
+stringNot s = do symbol . many1 $ noneOf s
+
+stringWSpaces :: Parser String
+stringWSpaces = do stringNot "\t\n"
 
 stringWOSpaces :: Parser String
 stringWOSpaces = do stringNot " \t\n"
 
-featsP :: Parser Feats
--- featsP' with sepBy will return the correct result for the empty
--- filed ('_'), but will report it the same as any other syntax error
-featsP = do mightBeEmpty [] featsP'
+---
+-- parser combinators
+keyValue :: Char -> Parser a -> Parser b -> Parser (a, b)
+keyValue sep p q = do key   <- p
+                      char sep
+                      value <- q
+                      return (key, value)
 
-mightBeEmpty :: a -> Parser a -> Parser a
-mightBeEmpty e p = do choice [notOption e emptyField, p]
+symbol :: Parser a -> Parser a
+symbol p = do litSpaces
+              x <- p
+              litSpaces
+              return x
 
-notOption :: a -> Parsec String u b -> Parsec String u a
-notOption x p = do r <- optionMaybe p
-                   case r of
-                     Just _ -> return x
-                     Nothing -> do parserZero
+maybeEmpty :: Parser a -> Parser (Maybe a)
+maybeEmpty p = do emptyField <|> liftM Just p
 
-emptyField :: Parser ()
-emptyField = do symbol $ char '_'
-                return ()
+listP :: Parser [a] -> Parser [a]
+-- using a parser that returns a possibly empty list like sepBy and
+-- many will return the correct result for the empty filed ('_'), but
+-- will report it the same as any other syntax error
+listP p = do liftM (fromMaybe []) $ maybeEmpty p
 
-featsP' :: Parser Feats
-featsP' = do sepBy1 stringPair (char '|')
-
-depheadP :: Parser Index
-depheadP = do headIx <- symbol $ many1 digit
-              return (read headIx :: Index)
-
-deprelP :: Parser DepRel
-deprelP = do liftM mkDepRel string
-
-depsP :: Parser Deps
-depsP = do mightBeEmpty [] depsP'
-
-depsP' :: Parser Deps
-depsP' = do sepBy1 (keyValue ':' index deprelP) (char '|')
-
-miscP :: Parser Misc
-miscP = do stringWSpaces
-
-{--
-parseLine' :: [String] -> Maybe Token
-parseLine' (s1:s2:s3:s4:s5:s6:s7:s8:s9:s10:_)
-  | isJust ix && all isDigit s7 && (na s9 || all isDigit s9) =
-      Just $ Token
-      { ix       = fromJust ix
-      , form     = s2
-      , lemma    = if s3==unk then [] else splitOn "|" s3
-      , cpostag  = s4
-      , postag   = s5
-      , feats    = if na s6 then [] else splitOn "|" s6
-      , dephead  = read s7
-      , deprel   = s8
-      , pdephead = if na s9  then Nothing else Just $ read s9
-      , pdeprel  = if na s10 then Nothing else Just s10 }
-  | otherwise   = Nothing
-  where ix = readIx s1
-        na = (`elem` ["_","-"])
-parseLine' _ = Nothing
-
-
--- skips sentences that contain unparsable lines
-readCorpusStr :: String -> Corpus
-readCorpusStr =
-  mapMaybe (fmap mkSentence . sequence . map parseLine) .
-  split (dropInitBlank . dropFinalBlank . dropDelims . condense $ whenElt emptyLine) .
-  lines
-  where emptyLine = null . words
-
-readCorpus :: FilePath -> IO Corpus
-readCorpus f = readCorpusStr <$> readFile f
-
-showCorpus :: Corpus -> String
-showCorpus = unlines . map (unlines . map showToken . sentenceTokens)
-  where showToken t = intercalate "\t" $ map (\f -> f t)
-          [show . ix, form, showLemma . lemma, cpostag,
-           postag, const "_", show . dephead, deprel,
-           fromMaybe "_" . fmap show . pdephead, fromMaybe "_" . pdeprel]
-        showLemma l | null l    = unk
-                    | otherwise = intercalate "|" l
-
-headToken :: Sentence -> Token -> Maybe Token
-headToken s t = case dephead t of
-              0 -> Nothing
-              i -> M.lookup i s
-
-sentenceTokens :: Sentence -> [Token]
-sentenceTokens = M.elems
-
-depTokens :: Sentence -> Token -> [Token]
-depTokens s t = filter ((==ix t) . dephead) $ M.elems s
-
-depTokensBy :: DepRel -> Sentence -> Token -> [Token]
-depTokensBy r s = filter ((==r) . deprel) . depTokens s
-
-adjTokens :: Sentence -> Token -> [Token]
-adjTokens s t = catMaybes [headToken s t] ++ depTokens s t
-
-getLP :: Token -> [LemmaPos]
-getLP t = [(l,cpostag t) | l <- lemma t]
-
--- fallbacks to wordform in case of unknown lemma
-getLP' :: Token -> [LemmaPos]
-getLP' t = if unknownLemma t then [(form t,postag t)] else getLP t
-
-showLP :: LemmaPos -> String
-showLP (l,[]) = l
-showLP (l,p)  = l ++ posSep ++ p
-
-lemmaPos :: Token -> [String]
-lemmaPos = map showLP . getLP
-
-lemma' :: Token -> [String]
-lemma' t = case lemma t of
-  [] -> [form t]
-  ls -> ls
-
-lemmaPos' :: Token -> [String]
-lemmaPos' = map showLP . getLP'
-
-formPos :: Token -> String
-formPos t = case (form t, cpostag t) of
-  (w,[]) -> w
-  (w,p)  -> w ++ posSep ++ p
-
-unknownLemma :: Token -> Bool
-unknownLemma = null . lemma
-
---}
+---
+-- main
+main :: IO ()
+main = do
+  [filename] <- getArgs
+  result <- parseFromFile document filename
+  case result of
+    Left err -> print err
+    Right ss -> putStr $ show ss
