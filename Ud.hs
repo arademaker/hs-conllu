09bf95dadb6fc83220f4b30a50089f465efe2352
odanoburu <bcclaro@gmail.com>
2018-01-06 15:11:57 -0200

    - mk deprel and postags uppercase - change func type

--- Ud.hs
+++ Ud.hs
@@ -10,8 +10,8 @@ module Ud where
 -- imports
 
 -- stdlib
+import Data.Char
 import Data.Maybe
-
 
 ---
 -- type and data declarations
@@ -33,7 +33,7 @@ data Token
            , feats   :: Feats
            , dephead :: Dephead
            , deprel  :: DepRel
-           , deps    :: Maybe Deps
+           , deps    :: Deps
            , misc    :: Misc
            }
   | MToken { start :: Index
@@ -50,7 +50,7 @@ data Token
            , xpostag  :: Xpostag
            , feats    :: Feats
            -- no head and deprel -- same as id token
-           , eDeps    :: Deps
+           , deps     :: Deps
            , misc     :: Misc
            -- field with prefix 'e' represents the fact that it is
            -- different from the one in SToken (can be empty in EToken
@@ -59,7 +59,7 @@ data Token
   deriving (Eq, Show)
 
 type Index   = Int
-type IxSep   = Maybe Char
+type IxSep   = Char
 type Form    = String
 type Lemma   = String
 type Xpostag = Maybe String
@@ -69,139 +69,140 @@ type Deps    = [(Index, DepRel)]
 type Misc    = Maybe String
 
 data DepRel
-  = Acl
-  | Advcl
-  | Advmod
-  | Amod
-  | Appos
-  | AuxDR
-  | Case
-  | Cc
-  | Ccomp
-  | Clf
-  | Compound
-  | Conj
-  | Cop
-  | Csubj
-  | Dep
-  | DetDR
-  | Discourse
-  | Dislocated
-  | Expl
-  | Fixed
-  | Flat
-  | Goeswith
-  | Iobj
-  | List
-  | Mark
-  | Nmod
-  | Nsubj
-  | Nummod
-  | Obj
-  | Obl
-  | Orphan
-  | Parataxis
-  | PunctDR
-  | Reparandum
-  | Root
-  | Vocative
-  | Xcomp
+  = ACL
+  | ADVCL
+  | ADVMOD
+  | AMOD
+  | APPOS
+  | AUXdr
+  | CASE
+  | CC
+  | CCOMP
+  | CLF
+  | COMPOUND
+  | CONJ
+  | COP
+  | CSUBJ
+  | DEP
+  | DETdr
+  | DISCOURSE
+  | DISLOCATED
+  | EXPL
+  | FIXED
+  | FLAT
+  | GOESWITH
+  | IOBJ
+  | LIST
+  | MARK
+  | NMOD
+  | NSUBJ
+  | NUMMOD
+  | OBJ
+  | OBL
+  | ORPHAN
+  | PARATAXIS
+  | PUNCTdr
+  | REPARANDUM
+  | ROOT
+  | VOCATIVE
+  | XCOMP
   deriving (Eq, Show)
 
 data PosTag
-  = Adj
-  | Adp
-  | Adv
-  | AuxPOS -- POS because there is a Aux in deprel
-  | Cconj
-  | DetPOS
-  | Intj
-  | Noun
-  | Num
-  | Part
-  | Pron
-  | Propn
-  | PunctPOS
-  | Sconj
-  | Sym
-  | Verb
+  = ADJ
+  | ADP
+  | ADV
+  | AUXpos -- pos because there is an aux in deprel
+  | CCONJ
+  | DETpos
+  | INTJ
+  | NOUN
+  | NUM
+  | PART
+  | PRON
+  | PROPN
+  | PUNCTpos
+  | SCONJ
+  | SYM
+  | VERB
   | X
   deriving (Eq, Show)
 
 ---
 -- constructor functions
-mkSentence :: [Comment] -> [Token] -> Sentence
-mkSentence cs ts = Sentence { meta = cs, tokens = ts }
-
 mkDepRel :: String -> DepRel
-mkDepRel "Acl"        = Acl
-mkDepRel "Advcl"      = Advcl
-mkDepRel "Advmod"     = Advmod
-mkDepRel "Amod"       = Amod
-mkDepRel "Appos"      = Appos
-mkDepRel "Aux"        = AuxDR
-mkDepRel "Case"       = Case
-mkDepRel "Cc"         = Cc
-mkDepRel "Ccomp"      = Ccomp
-mkDepRel "Clf"        = Clf
-mkDepRel "Compound"   = Compound
-mkDepRel "Conj"       = Conj
-mkDepRel "Cop"        = Cop
-mkDepRel "Csubj"      = Csubj
-mkDepRel "Dep"        = Dep
-mkDepRel "Det"        = DetDR
-mkDepRel "Discourse"  = Discourse
-mkDepRel "Dislocated" = Dislocated
-mkDepRel "Expl"       = Expl
-mkDepRel "Fixed"      = Fixed
-mkDepRel "Flat"       = Flat
-mkDepRel "Goeswith"   = Goeswith
-mkDepRel "Iobj"       = Iobj
-mkDepRel "List"       = List
-mkDepRel "Mark"       = Mark
-mkDepRel "Nmod"       = Nmod
-mkDepRel "Nsubj"      = Nsubj
-mkDepRel "Nummod"     = Nummod
-mkDepRel "Obj"        = Obj
-mkDepRel "Obl"        = Obl
-mkDepRel "Orphan"     = Orphan
-mkDepRel "Parataxis"  = Parataxis
-mkDepRel "Punct"      = PunctDR
-mkDepRel "Reparandum" = Reparandum
-mkDepRel "Root"       = Root
-mkDepRel "Vocative"   = Vocative
-mkDepRel "Xcomp"      = Xcomp
+mkDepRel = mkDepRel' . upcaseString
+  where
+    mkDepRel' "ACL"        = ACL
+    mkDepRel' "ADVCL"      = ADVCL
+    mkDepRel' "ADVMOD"     = ADVMOD
+    mkDepRel' "AMOD"       = AMOD
+    mkDepRel' "APPOS"      = APPOS
+    mkDepRel' "AUX"        = AUXdr
+    mkDepRel' "CASE"       = CASE
+    mkDepRel' "CC"         = CC
+    mkDepRel' "CCOMP"      = CCOMP
+    mkDepRel' "CLF"        = CLF
+    mkDepRel' "COMPOUND"   = COMPOUND
+    mkDepRel' "CONJ"       = CONJ
+    mkDepRel' "COP"        = COP
+    mkDepRel' "CSUBJ"      = CSUBJ
+    mkDepRel' "DEP"        = DEP
+    mkDepRel' "DET"        = DETdr
+    mkDepRel' "DISCOURSE"  = DISCOURSE
+    mkDepRel' "DISLOCATED" = DISLOCATED
+    mkDepRel' "EXPL"       = EXPL
+    mkDepRel' "FIXED"      = FIXED
+    mkDepRel' "FLAT"       = FLAT
+    mkDepRel' "GOESWITH"   = GOESWITH
+    mkDepRel' "IOBJ"       = IOBJ
+    mkDepRel' "LIST"       = LIST
+    mkDepRel' "MARK"       = MARK
+    mkDepRel' "NMOD"       = NMOD
+    mkDepRel' "NSUBJ"      = NSUBJ
+    mkDepRel' "NUMMOD"     = NUMMOD
+    mkDepRel' "OBJ"        = OBJ
+    mkDepRel' "OBL"        = OBL
+    mkDepRel' "ORPHAN"     = ORPHAN
+    mkDepRel' "PARATAXIS"  = PARATAXIS
+    mkDepRel' "PUNCT"      = PUNCTdr
+    mkDepRel' "REPARANDUM" = REPARANDUM
+    mkDepRel' "ROOT"       = ROOT
+    mkDepRel' "VOCATIVE"   = VOCATIVE
+    mkDepRel' "XCOMP"      = XCOMP
 
 mkPosTag :: String -> PosTag
-mkPosTag "Adj"      = Adj
-mkPosTag "Adp"      = Adp
-mkPosTag "Adv"      = Adv
-mkPosTag "Aux"      = AuxPOS
-mkPosTag "Cconj"    = Cconj
-mkPosTag "Det"      = DetPOS
-mkPosTag "Intj"     = Intj
-mkPosTag "Noun"     = Noun
-mkPosTag "Num"      = Num
-mkPosTag "Part"     = Part
-mkPosTag "Pron"     = Pron
-mkPosTag "Propn"    = Propn
-mkPosTag "Punct"    = PunctPOS
-mkPosTag "Sconj"    = Sconj
-mkPosTag "Sym"      = Sym
-mkPosTag "Verb"     = Verb
-mkPosTag "X"        = X
+mkPosTag = mkPosTag' . upcaseString
+  where
+    mkPosTag' "ADJ"      = ADJ
+    mkPosTag' "ADP"      = ADP
+    mkPosTag' "ADV"      = ADV
+    mkPosTag' "AUX"      = AUXpos
+    mkPosTag' "CCONJ"    = CCONJ
+    mkPosTag' "DET"      = DETpos
+    mkPosTag' "INTJ"     = INTJ
+    mkPosTag' "NOUN"     = NOUN
+    mkPosTag' "NUM"      = NUM
+    mkPosTag' "PART"     = PART
+    mkPosTag' "PRON"     = PRON
+    mkPosTag' "PROPN"    = PROPN
+    mkPosTag' "PUNCT"    = PUNCTpos
+    mkPosTag' "SCONJ"    = SCONJ
+    mkPosTag' "SYM"      = SYM
+    mkPosTag' "VERB"     = VERB
+    mkPosTag' "X"        = X
 
-mkToken :: Index -> IxSep -> Maybe Index -> Maybe Form
-  -> Maybe Lemma -> Maybe PosTag -> Xpostag -> Feats
-  -> Dephead -> DepRel -> Maybe Deps -> Misc -> Token
+mkToken :: Index -> Maybe IxSep -> Maybe Index -> Maybe Form
+  -> Maybe Lemma -> Maybe PosTag -> Xpostag -> Feats -> Maybe Dephead
+  -> Maybe DepRel -> Deps -> Misc -> Token
 mkToken ix sep = case sep of
   Nothing  -> mkSToken ix
   Just '-' -> mkMToken ix
   Just '.' -> mkEToken ix
 
 mkSToken :: Index -> Maybe Index -> Maybe Form -> Maybe Lemma
-  -> Maybe PosTag -> Xpostag -> Feats -> Dephead -> DepRel -> Maybe Deps
-  -> Misc -> Token
+  -> Maybe PosTag -> Xpostag -> Feats -> Maybe Dephead -> Maybe DepRel
+  -> Deps -> Misc -> Token
 mkSToken i ci fo l up xp fe h dr d m =
   SToken { ix      = i
          , form    = fromJust fo
@@ -209,21 +210,21 @@ mkSToken i ci fo l up xp fe h dr d m =
          , upostag = fromJust up
          , xpostag = xp
          , feats   = fe
-         , dephead = h
-         , deprel  = dr
+         , dephead = fromJust h
+         , deprel  = fromJust dr
          , deps    = d
          , misc    = m
          }
 
 mkMToken :: Index -> Maybe Index -> Maybe Form -> Maybe Lemma
-  -> Maybe PosTag -> Xpostag -> Feats -> Dephead -> DepRel -> Maybe Deps
-  -> Misc -> Token
+  -> Maybe PosTag -> Xpostag -> Feats -> Maybe Dephead -> Maybe DepRel
+  -> Deps -> Misc -> Token
 mkMToken s e fo l up xp fe h dr d m =
   MToken {start = s, end = fromJust e, form = fromJust fo, misc = m}
 
 mkEToken :: Index -> Maybe Index -> Maybe Form -> Maybe Lemma
-  -> Maybe PosTag -> Xpostag -> Feats -> Dephead -> DepRel -> Maybe Deps
-  -> Misc -> Token
+  -> Maybe PosTag -> Xpostag -> Feats -> Maybe Dephead -> Maybe DepRel
+  -> Deps -> Misc -> Token
 mkEToken i ci fo l up xp fe h dr d m =
   EToken
   { ix       = i
@@ -233,6 +234,11 @@ mkEToken i ci fo l up xp fe h dr d m =
   , eUpostag = up
   , xpostag  = xp
   , feats    = fe
-  , eDeps    = fromJust d
+  , deps     = d
   , misc     = m
   }
+
+---
+-- utility functions
+upcaseString :: String -> String
+upcaseString = map toUpper
