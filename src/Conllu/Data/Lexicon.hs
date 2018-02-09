1a9aefaa07bccb4d7a67812973c63a220cfe82c1
odanoburu <bcclaro@gmail.com>
2018-02-09 14:57:16 -0200

    - add InTrie data decl - correct memberLex

--- src/Conllu/Data/Lexicon.hs
+++ src/Conllu/Data/Lexicon.hs
@@ -19,6 +19,9 @@ data Trie a = Trie Bool (M.Map a (Trie a))
 
 type TTrie = Trie String
 
+data InTrie = No | Partially | Yes
+  deriving (Eq, Show)
+
 ---
 -- trie
 emptyMap :: M.Map k v
@@ -37,19 +40,25 @@ insertLex tt@(Trie v m) (x:xt) =
   let tt' = M.findWithDefault emptyTTrie x m
   in Trie v $ M.insert x (insertLex tt' xt) m
 
-memberLex :: TTrie -> [String] -> Bool
+memberLex :: TTrie -> [String] -> InTrie
 -- terrible code
-memberLex (Trie v _) [] = v
-memberLex (Trie _ m) [x] =
-  memberLex (M.findWithDefault emptyTTrie x m) []
+memberLex (Trie v _) [] = if v then Yes else Partially
 memberLex (Trie _ m) (x:xt) =
-  let tt = M.findWithDefault emptyTTrie x m
-  in if isEmptyTTrie tt
-       then False
-       else memberLex tt xt
+  let mt = M.lookup x m
+  in case mt of
+    Nothing -> No
+    Just t -> memberLex t xt
 
 ---
 -- recognizing
+recTks :: TTrie -> [Token] -> [Token]
+recTks tt (tk:tks) = case memberLex tt [fromJust $ _form tk] of
+  No -> recTks tt tks
+  Partially -> tk:(recTks tt tks) ++ recTks tt tks
+  Yes -> [tk] ++ recTks tt tks
+
+--recTksPartially :: TTrie -> [Token] -> [Token]
+--recTksPartially tt tks = 
 
 ---
 -- main
