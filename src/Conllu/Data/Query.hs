33356bf24b3d37e4cc6d3329874105d022e5e4df
odanoburu <bcclaro@gmail.com>
2018-03-01 18:51:21 -0300

    add study of query syntax

--- src/Conllu/Data/Query.hs
+++ src/Conllu/Data/Query.hs
@@ -1,6 +1,17 @@
+
 module Query where
 
 import Conllu.Type
 
-data CField = Id | Form deriving (Eq,Show)
-data Query = FieldQ CField | FieldValueQ CField String | AndQ [Query] | OrQ [Query] | NegQ Query
+type CField = String
+data Query
+  = FieldValueQ CField String -- ^  (deprel appos), (form brasil), (misc blablabla), (lemma *)
+  | AndQ [Query] -- ^ (and (deprel appos) (form mãe))
+  | OrQ [Query] -- ^ (or (id 3) (feats Tense=Past))
+  | NegQ Query -- ^ (not (deprel nsubj))
+  | HeadQ Query -- ^ (-> (deprel root))
+
+
+type QueryR = [([[Index]] -- ^ so that we can return singletons,
+                          -- pairs, triples, etc of tokens
+               , Sentence)]
