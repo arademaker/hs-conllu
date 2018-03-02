a5bff1a8a6e5c01694c28430d3f7bef39ded62c5
odanoburu <bcclaro@gmail.com>
2018-03-02 18:48:39 -0300

    - todo: update - query.org: add examples - query: try to model

--- src/Conllu/Data/Query.hs
+++ src/Conllu/Data/Query.hs
@@ -3,13 +3,23 @@ module Query where
 
 import Conllu.Type
 
+
+
 type CField = String
+
+data CQuery = CQuery [Query] [TQuery]
+
+data TQuery = HeadQ Query -- ^ (-> (deprel root))
+  | PrecedQ [Index]
+  | EqualQ [Query]
+  deriving (Eq,Show)
+
 data Query
   = FieldValueQ CField String -- ^  (deprel appos), (form brasil), (misc blablabla), (lemma *)
   | AndQ [Query] -- ^ (and (deprel appos) (form mãe))
   | OrQ [Query] -- ^ (or (id 3) (feats Tense=Past))
   | NegQ Query -- ^ (not (deprel nsubj))
-  | HeadQ Query -- ^ (-> (deprel root))
+  deriving (Eq,Show)
 
 
 type QueryR = [([[Index]] -- ^ so that we can return singletons,
